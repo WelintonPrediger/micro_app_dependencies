3e67059d6a563d547638bf3b393b8fdc398c2474
wpsilva <wpsliva@maisaedu.com.br>
2022-09-29 10:42:18 -0300

    Start project

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+export 'package:build_runner/build_runner.dart';
+export 'package:flutter_mobx/flutter_mobx.dart';
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:modular_test/modular_test.dart';
