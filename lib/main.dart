d8cc3a398d07890fbbd4ed54015b53c5418a2c7d
wpsilva <wpsliva@maisaedu.com.br>
2022-10-03 14:28:23 -0300

    Added Material like export file

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,4 @@ export 'package:build_runner/build_runner.dart';
 export 'package:flutter_mobx/flutter_mobx.dart';
 export 'package:flutter_modular/flutter_modular.dart';
 export 'package:modular_test/modular_test.dart';
+export 'package:flutter/material.dart';
