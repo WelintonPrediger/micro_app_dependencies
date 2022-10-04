43f540db37047232e61f50d0f0ad9104150aa463
wpsilva <wpsliva@maisaedu.com.br>
2022-10-04 11:13:53 -0300

    Adjust dev dependencies because it is not possible call it in another project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,2 @@
 export 'package:mobx/mobx.dart';
 export 'package:flutter_modular/flutter_modular.dart';
-export 'package:build_runner/build_runner.dart';
-export 'package:modular_test/modular_test.dart';
