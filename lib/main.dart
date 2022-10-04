891a3225a6fb49cfd90ac03fdc17fa81897c52a0
wpsilva <wpsliva@maisaedu.com.br>
2022-10-04 14:38:26 -0300

    Adjust export mobx and flutter_mobx

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,7 @@
-export 'package:flutter_mobx/flutter_mobx.dart';
+export 'package:mobx/mobx.dart';
 export 'package:flutter_modular/flutter_modular.dart';
+export 'package:flutter_mobx/src/observer.dart';
+export 'package:flutter_mobx/src/stateful_observer_widget.dart';
+export 'package:flutter_mobx/src/stateless_observer_widget.dart';
+export 'package:flutter_mobx/src/observer_widget_mixin.dart';
+export 'package:flutter_mobx/src/reaction_builder.dart';
