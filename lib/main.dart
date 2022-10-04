5a24e7f7c66694ac70f54c5defe4be9f1dd079ef
wpsilva <wpsliva@maisaedu.com.br>
2022-10-04 11:07:16 -0300

    Adjusted export files

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,4 @@
 export 'package:mobx/mobx.dart';
 export 'package:flutter_modular/flutter_modular.dart';
+export 'package:build_runner/build_runner.dart';
+export 'package:modular_test/modular_test.dart';
